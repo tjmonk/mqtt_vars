f0cdf825dab856278c98f253723fa7daa019bbc7
Trevor Monk <tjmonk@gmail.com>
2023-06-15 15:24:38 -0700

    add missing paho mqtt lib build

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+BASEDIR=`pwd`
+
+# get and build paho-mqtt
+git clone https://github.com/eclipse/paho.mqtt.c.git
+cd paho.mqtt.c
+make
+sudo make install
+
+# Build mqttvars
+cd $BASEDIR
 mkdir -p build && cd build
 cmake ..
 make
